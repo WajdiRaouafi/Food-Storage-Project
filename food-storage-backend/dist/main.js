3d97b5e28c3b5e2e48c4f5df6d3a2e1b657cbeb7
WajdiRaouafi <raouafiwajdi@outlook.com>
2025-02-07 17:07:43 +0100

    delete swagger

--- food-storage-backend/dist/main.js
+++ food-storage-backend/dist/main.js
@@ -2,7 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
-const swagger_1 = require("@nestjs/swagger");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.enableCors({
@@ -10,15 +9,7 @@ async function bootstrap() {
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
         credentials: true,
     });
-    const config = new swagger_1.DocumentBuilder()
-        .setTitle('Food-Storages')
-        .setDescription('The Food Storage API provides endpoints to manage food items in a storage system. Users can perform CRUD (Create, Read, Update, Delete) operations on food items, including adding new items, retrieving existing items, updating item information, and removing items from storage.')
-        .setVersion('1.3')
-        .addTag('food-storages')
-        .build();
-    const document = swagger_1.SwaggerModule.createDocument(app, config);
-    swagger_1.SwaggerModule.setup('api', app, document);
-    await app.listen(3000);
+
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
